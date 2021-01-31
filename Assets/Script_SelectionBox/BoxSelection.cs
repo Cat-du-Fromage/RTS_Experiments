622e1cbb9a416f60dd5bd6fe04c8145fb87c27a3
Florian <florian.duruz@gmail.com>
2021-01-31 17:44:02 +0100

    init

---
+++ Assets/Script_SelectionBox/BoxSelection.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSelection : MonoBehaviour
+{
+    [SerializeField] Collider[] selections; // Indicator in the Unity UI: number of object selected
+    [SerializeField] Box box; //indicator in the Unity UI so we can see the box value
+
+    private Vector3 startPosition; //mouse click position start
+    private Vector3 dragPosition;
+
+    private Camera cam;
+    private Ray ray; //distance  between 2 points
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main; //we select the main camera
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            Physics.Raycast(ray, out hit , 100f);
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                //On Drag start
+                startPosition = hit.point;
+                box.baseMin = startPosition;
+
+
+            }
+            //When dragging the mouse
+            dragPosition = hit.point;
+            box.baseMax = dragPosition;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            //When mouse released
+            selections = Physics.OverlapBox(box.Center, box.Extents, Quaternion.identity);
+            box.baseMin = new Vector3(0, 0, 0);
+            box.baseMax = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireCube(box.Center, box.Size);
+    }
+}
+
+[System.Serializable]
+public class Box
+{
+    public Vector3 baseMin; //startClick position
+    public Vector3 baseMax; //encClick position
+
+    public Vector3 Center
+    {
+        get
+        {
+            Vector3 center = baseMin + (baseMax - baseMin) * 0.5f;
+            center.y = (baseMax - baseMin).magnitude * 0.5f;
+            return center;
+        }
+    }
+
+    public Vector3 Size
+    {
+        get
+        {
+            return new Vector3(Mathf.Abs(baseMax.x - baseMin.x), (baseMax - baseMin).magnitude, Mathf.Abs(baseMax.z - baseMin.z));
+        }
+    }
+
+    public Vector3 Extents
+    {
+        //needed for Collider (go to OnDrawGizmo)
+        get
+        {
+            return Size * 0.5f;
+        }
+    }
+}
---
+++ Assets/Script_SelectionBox/Selector_Unit.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Drawing; // POUR RECTANGLEF
+
+public class Selector_Unit : MonoBehaviour
+{
+    [Header("For Raycast")]
+    public Camera cam;
+    public LayerMask mask;
+
+    [Header("Selection Box start/end points")]
+    public Vector3 startPosition; //mouse click position start
+    public Vector3 endPosition;
+
+    [Header("Selection Box Info")]
+    public RectangleF rect_selectionRect;
+    public Vector3 rect_Center;
+    public Vector3 rect_Size;
+    public Vector3 rect_halfExtents;
+
+    [Header("All Currently Selected Units")]
+    public List<GameObject> selectedUnits;
+
+    [Header("GameObject for selection box")]
+    public GameObject selectorBox;
+
+    private Vector3 movetoPos = Vector3.zero; //pour mouvement donc PAS MAINTENANT
+
+    [Header("Formation Unit")]
+
+    //public Transform spawnPosition;
+    public GameObject Unit_FormationIndicatorSize;
+    public List<GameObject> UnitFormationIndicators;
+
+    //limit Row
+    public int maxRowFormation = 10; // dépendra du type d'unité(class_unit.maxRow)
+    public int minRowFormation = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnitFormationIndicators = new List<GameObject>();
+        selectedUnits = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPosition = DoRay();
+            selectorBox.SetActive(true);
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            endPosition = DoRay();
+            HandleRectangle();
+            selectorBox.transform.position = rect_Center;
+            selectorBox.transform.localScale = rect_Size + new Vector3(0f,1f,0f);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            selectorBox.SetActive(false);
+            endPosition = DoRay();
+            HandleRectangle();
+            SelectAllUnits();
+        }
+        //Mouvement ICI
+        if(selectedUnits.Count > 0)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                startPosition = DoRay();
+                //Give the correct form to the formation tool depending of the scale of the unit selected
+                Unit_FormationIndicatorSize = UnitFormationCylinder(selectedUnits[0].transform);
+
+                Vector3 newPlacement = startPosition;
+                for (int i = 0; i < selectedUnits.Count; i++)
+                {
+                    GameObject clone;
+                    if (i % minRowFormation == 0)
+                    {
+                        newPlacement.x = startPosition.x;
+                        newPlacement.z -= selectedUnits[0].transform.localScale.z;
+                        clone = Instantiate(Unit_FormationIndicatorSize, newPlacement, Quaternion.identity);
+                        UnitFormationIndicators.Add(clone.gameObject);
+                    }
+                    else
+                    {
+                        newPlacement.x += (selectedUnits[0].transform.localScale.x) + 0.1f;
+                        clone = Instantiate(Unit_FormationIndicatorSize, newPlacement, Quaternion.identity);
+                        UnitFormationIndicators.Add(clone.gameObject);
+                    }
+                }
+
+            }
+            if (Input.GetMouseButton(1))
+            {
+                endPosition = DoRay();
+                Ray mousDrag = new Ray(startPosition, endPosition);
+                Vector3 newPlacement = startPosition;
+                for (int i = 0; i < UnitFormationIndicators.Count; i++)
+                {
+                    if (i % minRowFormation == 0 )
+                    {
+                        newPlacement.x = startPosition.x - 1f;
+                        newPlacement.z = startPosition.z + 1f;
+                        UnitFormationIndicators[i].transform.position = newPlacement;
+                    }
+                    else
+                    {
+                        newPlacement.x += 1f;
+                        newPlacement.z += 1f;
+                        UnitFormationIndicators[i].transform.position = newPlacement;
+                    }
+                }
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                foreach(GameObject clone in UnitFormationIndicators)
+                {
+                    Destroy(clone.gameObject);
+                }
+
+                UnitFormationIndicators.Clear();
+            }
+        }
+    }
+    private GameObject UnitFormationCylinder(Transform Unitscale)
+    {
+        //a terme DEVRA repéré cavalerie = triangle; troupe à pied = cylindre
+        Unit_FormationIndicatorSize = GameObject.Find("Unit_FormationIndicator");
+        Unit_FormationIndicatorSize.transform.localScale = Unitscale.localScale;
+
+        return Unit_FormationIndicatorSize;
+    }
+
+    private void HandleRectangle()
+    {
+        rect_Size = startPosition - endPosition;
+        rect_Size.x = Mathf.Abs(rect_Size.x);
+        rect_Size.y = Mathf.Abs(rect_Size.y);
+        rect_Size.z = Mathf.Abs(rect_Size.z);
+
+        rect_Center = (startPosition + endPosition) * 0.5f;
+
+        rect_halfExtents = rect_Size * 0.5f;
+    }
+
+    private void ClearAllUnits()
+    {
+        for(int i = 0; i < selectedUnits.Count; i++)
+        {
+            Units unit = selectedUnits[i].GetComponent<Units>();
+            // changement de couleur ici normalement
+            Renderer rSelect = selectedUnits[i].GetComponent<Renderer>();
+            Material mSelect = rSelect.material;
+            mSelect.color = UnityEngine.Color.blue;
+        }
+        //Remove every component in the list
+        selectedUnits.Clear();
+    }
+
+    private void SelectAllUnits()
+    {
+        ClearAllUnits();
+
+        RaycastHit[] check = Physics.BoxCastAll(rect_Center, rect_halfExtents, Vector3.up);
+        //verify that colliders are units
+        for(int i = 0; i < check.Length; i++)
+        {
+            if(check[i].collider.CompareTag("Unit"))
+            {
+                Renderer rSelect = check[i].collider.GetComponent<Renderer>();
+                Units unit = check[i].collider.GetComponent<Units>();
+                // changement de couleur ici normalement
+                Material mSelect = rSelect.material;
+                mSelect.color = UnityEngine.Color.red;
+
+                selectedUnits.Add(unit.gameObject);
+            }
+        }
+    }
+
+    private Vector3 DoRay()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit,1000f,mask))
+        {
+            if(hit.collider.CompareTag("Ground"))
+            {
+                return hit.point;
+            }
+        }
+
+        return Vector3.zero;
+    }
+
+    private void OnDrawGizmos()
+    {
+        //cube we draw
+        Gizmos.color = UnityEngine.Color.green;
+        Gizmos.DrawWireCube(rect_Center, rect_Size);
+        //start point of the cube
+        Gizmos.color = UnityEngine.Color.yellow;
+        Gizmos.DrawWireSphere(startPosition, 0.5f);
+        //end point of the cube
+        Gizmos.color = UnityEngine.Color.red;
+        Gizmos.DrawWireSphere(endPosition, 0.5f);
+        // POUR MOUVEMENT
+        /*
+        Gizmos.color = UnityEngine.Color.cyan;
+        Gizmos.DrawSphere(movetoPos, 0.5f);
+        */
+    }
+}
