ed78173b0f0cf5179ee2a6b879e72cd16c8a3cdf
Florian <florian.duruz@gmail.com>
2021-02-08 16:02:48 +0100

    Json_list + units object added

--- Assets/Units/JSON_UnitsList.cs
+++ Assets/Units/JSON_UnitsList.cs
@@ -7,7 +7,7 @@ using System;
 
 public class JSON_UnitsList : MonoBehaviour
 {
-    public UnitsList UnitsList = new UnitsList();
+    //public UnitsList UnitsList = new UnitsList();
     public TextAsset textJSON;
     public List<object> Unitslists = new List<object>();
     // Start is called before the first frame update
@@ -16,48 +16,17 @@ public class JSON_UnitsList : MonoBehaviour
         textJSON = Resources.Load("UnitsList") as TextAsset;
         if (textJSON != null)
         {
-            //Dictionary<string, object> json = JsonConvert.DeserializeObject<Dictionary<string, object>>(textJSON.text);
             Dictionary<string, object> json = JsonConvert.DeserializeObject<Dictionary<string, object>>(textJSON.text);
             JArray jsonUnits = (JArray)json["Units"];
             foreach (JObject a in jsonUnits)
             {
-                Unitslists.Add(jsonUnits);
-                print(a["name"]);
-                print(a["LifePoint"]);
-                print(a["SizeRegiment"]);
-            }
-
-            Debug.Log(Unitslists);
-            Debug.Log(json);
-        }
-        else
-        {
-            print("PAS de JSON");
-        }
-
-        /*
-        textJSON = Resources.Load("UnitsList") as TextAsset;
-        if(textJSON != null)
-        {
-            UnitsList = JsonUtility.FromJson<UnitsList>(textJSON.text);
-            foreach(Units unit in UnitsList.Units)
-            {
-                print(unit.Name);
-                print(unit.LifePoint);
-                print(unit.SizeRegiment);
+                Unitslists.Add(new Units(a["name"].ToString(), (int)a["CombatMelee"], (int)a["CombatRange"], (int)a["LifePoint"], (int)a["SizeRegiment"]));
             }
         }
         else
         {
             print("PAS de JSON");
         }
-        */
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
 
--- Assets/Units/JsonUSE/Units.cs
+++ Assets/Units/JsonUSE/Units.cs
@@ -7,24 +7,71 @@ using System;
 [System.Serializable]
 public class Units
 {
-    public string Name;
-    public int CombatMelee;
-    public int CombatRange;
-    public int LifePoint;
-    public int SizeRegiment;
+    #region Attributs
+    private string _name { get;}
+    private int _combatMelee { get;}
+    private int _combatRange { get;}
+    private int _lifePoint { get; set; }
+    private int _sizeRegiment { get; }
+    #endregion Attributs
+    /// <summary>
+    /// Constructor of a single Unit (not the group)
+    /// </summary>
+    /// <param name="_name">Unit Name</param>
+    /// <param name="_combatMelee">Melee combat value</param>
+    /// <param name="_combatRange">Range combat value</param>
+    /// <param name="_lifePoint">life point</param>
+    /// <param name="_sizeRegiment">size of a regiment/group</param>
+    #region Constructor
+    public Units(string _name, int _combatMelee, int _combatRange, int _lifePoint, int _sizeRegiment)
+    {
+        this._name = _name;
+        this._combatMelee = _combatMelee;
+        this._combatRange = _combatRange;
+        this._lifePoint = _lifePoint;
+        this._sizeRegiment = _sizeRegiment;
+    }
+    #endregion Constructor
 
-    public Units(string Name, int CombatMelee, int CombatRange, int LifePoint, int SizeRegiment)
+    #region Methods
+    #endregion Methods
+
+    #region Accessors
+    /// <summary>
+    /// Unit Name
+    /// </summary>
+    public string Name
     {
-        this.Name = Name;
-        this.CombatMelee = CombatMelee;
-        this.CombatRange = CombatRange;
-        this.LifePoint = LifePoint;
-        this.SizeRegiment = SizeRegiment;
+        get { return _name; }
     }
-    /*
-    public static Units fromDictionnary(Dictionary<string, object> json)
+    /// <summary>
+    /// Unit combat value when attacking in melee
+    /// </summary>
+    public int CombatMelee
     {
-        return Units(json["Name"], json["CombatMelee"], json["CombatRange"], json["LifePoint"], json["SizeRegiment"]);
+        get { return _combatMelee; }
     }
-    */
+    /// <summary>
+    /// Unit combat value when attacking at range
+    /// </summary>
+    public int CombatRange
+    {
+        get { return _combatRange; }
+    }
+    /// <summary>
+    /// how many life point the Unit has
+    /// </summary>
+    public int LifePoint
+    {
+        get { return _lifePoint; }
+        set { _lifePoint = value; }
+    }
+    /// <summary>
+    /// return how many unit compose a regiment/group
+    /// </summary>
+    public int SizeRegiment
+    {
+        get { return _sizeRegiment; }
+    }
+    #endregion Accessors
 }
